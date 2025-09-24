50f1c3feb655bcaff4f564eb536d5a2c9c264a31
juaniwk3 <86119976+JuanIWK3@users.noreply.github.com>
2025-09-24 00:51:41 -0300

    Replace date input with calendar popover
    The commit replaces a basic date input with a more polished calendar
    popover component, enabling easier date selection in the weight entry
    form.

--- components/add-weight.tsx
+++ components/add-weight.tsx
@@ -1,18 +1,26 @@
-import { revalidatePath } from "next/cache";
+"use client";
+
 import { addWeightEntry } from "@/actions/weight";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import React from "react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
+import { CalendarIcon } from "lucide-react";
+import { format } from "date-fns";
 
 export default function WeightForm({ userId }: { userId: string }) {
+  const [date, setDate] = React.useState<Date>();
+
   return (
     <form
       action={async (formData) => {
-        "use server";
         const value = Number(formData.get("value"));
-        const date = formData.get("date")?.toString();
-        console.log(date);
         await addWeightEntry(userId, value, date ? new Date(date) : new Date());
-        revalidatePath("dashboard");
       }}
       className="w-full flex flex-col items-center gap-2 max-w-sm"
     >
@@ -22,14 +30,23 @@ export default function WeightForm({ userId }: { userId: string }) {
         step={0.01}
         placeholder="Enter weight"
         required
-        className="border w-full rounded px-2 py-1"
+        className=""
       />
-      <Input
-        type="date"
-        name="date"
-        defaultValue={Date.now()}
-        className="border rounded w-full px-2 py-1"
-      />
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            data-empty={!date}
+            className="data-[empty=true]:text-muted-foreground w-full justify-start text-left font-normal"
+          >
+            <CalendarIcon />
+            {date ? format(date, "PPP") : <span>Pick a date</span>}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-0">
+          <Calendar mode="single" selected={date} onSelect={setDate} />
+        </PopoverContent>
+      </Popover>
       <Button type="submit" className="w-full">
         Add Entry
       </Button>
